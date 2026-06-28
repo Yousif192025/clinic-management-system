204e4ea5fe1c45607547e36b193274fd4e5e459c
Yousif192025 <alahli192024@gmail.com>
2026-06-28 15:35:55 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,11 @@
-import type { NextConfig } from 'next'
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'avatars.githubusercontent.com' },
-      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
-    ],
-  },
 }
 
-export default nextConfig
+module.exports = nextConfig
